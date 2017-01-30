1cbd574cacd7c51e3a21a1209506d8bd8852bdbe
Arin Arjani <arinarjani@gmail.com>
2017-01-30 13:27:52 -0800

    got fetch working and changed some css

--- script.js
+++ script.js
@@ -5,28 +5,37 @@
 //
 // var albumId = '2ODvWsOgouMbaA5xf0RkJe';
 
-// set up an obect
-var test = new XMLHttpRequest();
-// set a GET request
-test.open("GET", "https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj");
-// send the GET request
+var uri = "https://api.spotify.com/v1/search?q=the+killers&type=album";
 
-//test.setRequestHeader("Accept", "application/json");
-//test.send(null); // maybe need "null", not sure (works without it)
-// set a variable to the responseText and turn it into an Object
-//test.setRequestHeader('Accept','text/JSON');
-//test.send(null);
+// create shadow when clicking the search box
+var searchBox = document.getElementById('searchBox').onclick = function() {
+  this.style.boxShadow = "0px 3px 5px rgba(0,0,0, 0.5)";
+}
 
-//console.log(test.response.album);
+fetch(uri).then(function(response) {
+  return response.json();
+}).then(function(response) {
+  console.log("response in the second then:",response);
+
+  // get the results div from index.html
+  var resultsDiv = document.querySelector('#results');
+  var musicData = response;
+  var albumImg = musicData.albums.items[0].images[0].url;
 
 
-//var testObj = JSON.parse(test.responseText);
-//console.log(testObj);
+  // need to display data from the response param
+  var img = document.querySelector('.main-img');
+  img.src=albumImg;
 
-// var searchBox = document.getElementById('searchBox').onclick = function() {
-//   this.style.boxShadow = "0px 3px 5px rgba(0,0,0, 0.5)";
-// }
+  var index = 0;
+  while( !(musicData.albums.items[index].images[0].url) ) {
+    console.log("working");
+    index++;
+  }
 
-notes: fetch ( use promises (access from them when good) )
-- the problem now is the code is running without getting what it needs, so you get nothing or undefined
-- fetch will place the info in a placeholder, then once it gets the info, it will give me what I want ( something like that )
+});
+// notes: fetch ( use promises (access from them when good) )
+// - the problem now is the code is running without getting what it needs, so you
+//   get nothing or undefined
+// - fetch will place the info in a placeholder, then once it gets the info, it
+//   will give me what I want ( something like that )
